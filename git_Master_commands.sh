769cbfd73653b4a886fd22d783791cff63d8c255
Mahesh Arelli <Mahesh.Arelli@qolsys.com>
2021-02-17 18:35:52 +0530

    Files are added

---
+++ git_Master_commands.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git checkout master
+git branch
+ls
+git merge test
+ls
+git push origin master
+
+
--- git_Test_commands.sh
+++ git_Test_commands.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+git checkout test
 git branch
 ls
 git add . 
 git commit -m "Files are added"
 git status
-git push origin master
+git push origin test
 
 
